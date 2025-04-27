8a9f541ad5d9c12b815cd17d7a8695f107a2f686
ciengrantz <rhysgangaram213@gmail.com>
2025-04-27 13:30:02 -0400

    reviews are broken, lint js fixed...

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -42,7 +42,7 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-        year = models.IntegerField(default=2025,
+    year = models.IntegerField(default=2025,
         validators=[
             MaxValueValidator(2025),
             MinValueValidator(2000)
