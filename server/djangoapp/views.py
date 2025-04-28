0ad015d48d19ad0d614022c7efd2dc3d6cd5e434
ciengrantz <rhysgangaram213@gmail.com>
2025-04-28 12:05:27 -0400

    reviews are broken and jslint is fixed...git add --a!

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -72,8 +72,8 @@ def registration(request):
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
-              first_name=first_name,
-              last_name=last_name, password=password, email=email)
+                first_name=first_name,
+                last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -122,11 +122,11 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+# deleted            response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
-            print(f"Unexpected {err=}, {type(err)=}")
-            return JsonResponse({"status": 401,
+              print(f"Unexpected {err=}, {type(err)=}")
+              return JsonResponse({"status": 401,
                  "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403,
@@ -155,5 +155,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+             "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
