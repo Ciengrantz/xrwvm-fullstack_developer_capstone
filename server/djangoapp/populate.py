b889d834475fa1a33f2178db97d1b4675ceda71f
ciengrantz <rhysgangaram213@gmail.com>
2025-04-29 06:59:04 -0400

    lint 6

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,8 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
-             CarMake.objects.create(name=data['name'], description=data['description']))
+             CarMake.objects.create(name=data['name'],
+             description=data['description']))
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {"name": "Pathfinder", "type": "SUV",
@@ -46,7 +47,7 @@ def initiate():
        "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[4]},
-      
+    
     # Add more CarModel instances as needed
 
     ]
@@ -54,3 +55,4 @@ def initiate():
     for data in car_model_data:
         CarModel.objects.create(name=data['name'], car_make=data['car_make'],
              type=data['type'], year=data['year'])
+    
