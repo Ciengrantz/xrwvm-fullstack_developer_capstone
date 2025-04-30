6ee338872b780ef6dae759ac5f18b6b7097c1b43
ciengrantz <rhysgangaram213@gmail.com>
2025-04-30 08:16:01 -0400

    lint 13

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -43,10 +43,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2025,
-             validators=[
-                MaxValueValidator(2025),
-                MinValueValidator(2000)
-            ])
+                               validators=[
+                                    MaxValueValidator(2025),
+                                    MinValueValidator(2000)
+                                ])
     # Other fields as needed
 
     def __str__(self):
