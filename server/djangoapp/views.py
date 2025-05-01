534d35e4b4867a1d77440b69482d37faff0f5946
ciengrantz <rhysgangaram213@gmail.com>
2025-05-01 13:53:03 -0400

    reviews are fixed

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -137,17 +137,18 @@ def add_review(request):
                              "message": "Unauthorized"})
 
 
-def get_dealer_reviews(request, reviews, dealer_id):
-    if (dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
+    if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
+        return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+            return JsonResponse({"status":400,"message":"Bad Request"})
 
 
 # get cars:
